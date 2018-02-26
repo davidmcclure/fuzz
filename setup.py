31390f5f1a636427d930ebc629c925e74ccb00ae
David McClure <davidwilliammcclure@gmail.com>
2018-02-26 17:56:50 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='infuzzy',
+    version='0.1.0',
+    description='Fuzzy textual influence.',
+    license='MIT',
+    author='David McClure',
+    author_email='dclure@mit.edu',
+    packages=find_packages(),
+)
