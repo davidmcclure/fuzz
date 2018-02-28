6a3be279d0335900c7316487c8a9aa91e5a27cd9
David McClure <davidwilliammcclure@gmail.com>
2018-02-28 14:57:41 -0500

    Stub in Flask app.

---
+++ infuzzy/api.py
@@ -0,0 +1,17 @@
+
+
+import os
+
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000), debug=True)
